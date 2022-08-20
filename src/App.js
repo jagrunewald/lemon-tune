f849b7c7c87fde21d9c7430cf78f8d28dde8a895
jagrunewald <je_grunewald@hotmail.com>
2022-08-20 18:40:59 -0300

    Created routes for Login and Music pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import * as React from "react";
+import { Routes, Route } from 'react-router-dom';
+import Login from '../src/Pages/Login/Login';
+import Musics from '../src/Pages/Musics/Musics';
 
 function App() {
   return (
     <div>
-      
-    </div>      
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/musics" element={<Musics />} />
+      </Routes>
+    </div>
   );
 }
 
