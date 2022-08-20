f40f94ebe05fc14d09c55b3082131b9f317cfdb8
jagrunewald <je_grunewald@hotmail.com>
2022-08-20 18:58:32 -0300

    Adds new input for User Name or Nickname on Login page

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -5,6 +5,7 @@ import './Login.css';
 
 export default function Login () {
   const navigate = useNavigate();
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [buttonLogin, setButtonLogin] = useState(true);
@@ -13,12 +14,16 @@ export default function Login () {
     const regex = /.+@.+\.[A-Za-z]+$/;
     const minLength = 5;
 
-    if (password.length >= minLength && regex.test(email)) {
+    if (name !== '' && regex.test(email) && password.length >= minLength) {
       setButtonLogin(false);
     } else {
       setButtonLogin(true);
     }
-  }  
+  }
+
+  function handleName({ target }) {
+    setName(target.value);
+  }
 
   function handleEmail({ target }) {
     setEmail(target.value);
@@ -31,13 +36,15 @@ export default function Login () {
 
   function handleClick() {
     localStorage.setItem('user', JSON.stringify(email));
-    navigate("/musics");
+    navigate('/musics');
   }
 
   return (
     <div className='login'>
       <Header />
       <div className='login-container'>
+        <label htmlFor='name' className='label'>Name or Nickname</label>
+        <input type='text' className='input' id='name' name='name' value={name} onChange={ handleName } />
         <label htmlFor='email' className='label'>E-mail</label>
         <input type='email' className='input' id='email' name='email' value={email} onChange={ handleEmail } />
         <label htmlFor='password' className='label'>Password</label>
