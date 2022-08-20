fb02f226ba307445a6c3d2af34aae8fd6019e194
jagrunewald <je_grunewald@hotmail.com>
2022-08-20 18:37:46 -0300

    Create page Musics

---
+++ src/Pages/Musics/Musics.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Musics.css';
+import Header from '../../Components/Header/Header';
+
+export default function Musics () {
+  return (
+    <div className='musics'>
+      <Header />
+      Musics
+    </div>
+  )
+}
