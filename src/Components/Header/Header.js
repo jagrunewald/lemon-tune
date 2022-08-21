46fe4c160b492dc0fefd6c3d65a175a7045424bd
jagrunewald <je_grunewald@hotmail.com>
2022-08-21 13:07:30 -0300

    :recycle: refactor: CHANGE: Retrieve and add username on Musics page

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,21 +3,10 @@ import './Header.css';
 import lemon from '../../img/lemon-icon.png';
 
 export default function Header () {
-  function getUser () {
-    var nameUser = JSON.parse(localStorage.getItem('name'));
-
-    if(nameUser) {
-      return nameUser;
-    } else {
-      return '';
-    }
-  }
-
   return (
     <div className='header'>
       <img src={lemon} alt='lemonimg' className='header-icon'/>
-      <h1 className='header-title'>Lemon Tune</h1>
-      <p className='header-name'>{ getUser() }</p>
+      <h1 className='header-title'>Lemon Tune</h1>     
     </div>
   )
 }
--- src/Pages/Musics/Musics.js
+++ src/Pages/Musics/Musics.js
@@ -3,10 +3,26 @@ import './Musics.css';
 import Header from '../../Components/Header/Header';
 
 export default function Musics () {
+  function getUser () {
+    var nameUser = JSON.parse(localStorage.getItem('name'));
+
+    if(nameUser) {
+      return nameUser;
+    } else {
+      return '';
+    }
+  }
+
   return (
     <div className='musics'>
       <Header />
-      Musics
+      <div className='musics-page'>
+        <div className='musics-search'>
+          <h2 className='musics-name'>{ getUser() }</h2>
+        </div>
+        <div className='musics-all'>Teste</div>
+      </div>
+
     </div>
   )
 }
