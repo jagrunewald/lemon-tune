ab264899d61f0ae1d6003562ad312a9521fec87c
jagrunewald <je_grunewald@hotmail.com>
2022-08-21 14:03:14 -0300

    :sparkles: feat: Add input and search button on Musics page

--- src/Pages/Musics/Musics.js
+++ src/Pages/Musics/Musics.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Musics.css';
 import Header from '../../Components/Header/Header';
 
 export default function Musics () {
+  const [search, setSearch] = useState('');
+
+  function handleChange({ target }) {
+    setSearch(target.value);
+  }
+
+  function handleClick () {
+    console.log('clicado');
+  }
+
   function getUser () {
     var nameUser = JSON.parse(localStorage.getItem('name'));
 
@@ -17,12 +27,16 @@ export default function Musics () {
     <div className='musics'>
       <Header />
       <div className='musics-page'>
-        <div className='musics-search'>
+        <div className='musics-nav'>
           <h2 className='musics-name'>{ getUser() }</h2>
           <a href='' className='musics-favorites'>Favorites</a>
           <a href='' className='musics-logoff'>Logoff</a>
         </div>
-        <div className='musics-all'>Teste</div>
+        <div className='musics-all'>
+          <input type='text' className='input-search' value={search} onChange={ handleChange }></input>
+          <button type='button' className='button-search' onClick={ handleClick }>Search</button>
+          <div className='musics-all-response'></div>
+        </div>
       </div>
 
     </div>
