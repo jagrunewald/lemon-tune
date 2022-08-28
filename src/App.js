d3f2728aacfd74afefc53c470beb7610fb212047
jagrunewald <je_grunewald@hotmail.com>
2022-08-28 16:57:01 -0300

    Add Favorites page and /favorites route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import * as React from "react";
 import { Routes, Route } from 'react-router-dom';
 import Login from '../src/Pages/Login/Login';
 import Albums from '../src/Pages/Albums/Albums';
-import Musics from "./Pages/Musics/Musics";
+import Musics from './src/Pages/Musics/Musics';
+import Favorites from '../src/Pages/Favorites/Favorites';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/albums" element={<Albums />} />
         <Route path="/musics" element={<Musics />} />
+        <Route path="/favorites" element={<Favorites />} />
       </Routes>
     </div>
   );
---
+++ src/Pages/Favorites/Favorites.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Favorites.css';
+import Header from '../../Components/Header/Header';
+import Nav from '../../Components/Nav/Nav';
+
+export default function Favorites () {  
+  return (
+    <div className='favorites'>
+      <Header />
+      <Nav />
+    </div>
+  )
+    
+}
