16b1bcf450ceefe3b81b606aea2d6c5eda7a41f3
jagrunewald <je_grunewald@hotmail.com>
2022-09-04 11:32:47 -0300

    deploy

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -41,6 +41,7 @@ export default function Login () {
   function handleClick() {
     localStorage.setItem('user', JSON.stringify(email));
     localStorage.setItem('name', JSON.stringify(name));
+
     navigate('/albums');
   }
 
